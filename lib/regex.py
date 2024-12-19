e7f80b44df82b0b5b589e0f5ed1d8dd48e4d3802
MBURU <wawerumburu157@gmail.com>
2024-12-19 07:32:07 +0300

    changes

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,13 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
-
-name = r""
+# Updated Name regex with negative lookahead to disallow multiple spaces
+name = r"^(?!.*\s{2,})[A-Z][a-zA-Z' -]*(?: [A-Z][a-zA-Z' -]*)*$"
 name_regex = re.compile(name)
 
-phone_number = r""
+# Phone number regex: Various formats of 10-digit phone numbers
+phone_number = r"^(?:\(?\d{3}\)?[-.\s]?|\d{3}[-.\s]?)?\d{3}[-.\s]?\d{4}$"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+# Email address regex: Standard email formats starting with an alphabet
+email_address = r"^[a-zA-Z][a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
 email_regex = re.compile(email_address)
